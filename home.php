2afe24f094901af3be4775743830968271baf973
everaldomatias <matiaseveraldo@gmail.com>
2015-01-24 11:55:05 -0200

    Inicia sprint no content da Home Refs #3

--- home.php
+++ home.php
@@ -13,7 +13,18 @@ get_header( 'home' ); ?>
 	<div id="primary" class="<?php echo odin_classes_page_full(); ?>">
 		<div id="content" class="site-content" role="main">
 
-			Template para Home
+			<h1 class="entry-title"><?php _e( 'Destaques', 'odin' ); ?></h1>
+			<div class="boxes">
+				<div class="col-md-4">
+					Serviços
+				</div>
+				<div class="col-md-4">
+					Aceleração Social
+				</div>
+				<div class="col-md-4">
+					Projetos
+				</div>
+			</div><!-- boxes -->
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
