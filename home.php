7129490d53f71794f35521aa59746f7cd1319577
everaldomatias <matiaseveraldo@gmail.com>
2015-01-24 08:27:41 -0200

    Adiciona template home

---
+++ home.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that display Home.
+ *
+ * @package Odin
+ * @since 2.2.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="<?php echo odin_classes_page_full(); ?>">
+		<div id="content" class="site-content" role="main">
+
+			Template para Home
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
