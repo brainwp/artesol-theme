82b6881a450b65b8a87821749087307e8ae59d80
everaldomatias <matiaseveraldo@gmail.com>
2015-03-06 20:55:57 -0300

    Aplica CSS do box Servicos e Agenda

--- home.php
+++ home.php
@@ -16,7 +16,25 @@ get_header( 'home' ); ?>
 			<h1 class="entry-title"><?php _e( 'Destaques', 'odin' ); ?></h1>
 			<div class="boxes">
 				<div class="col-md-4">
-					Serviços
+
+					<div class="servicos">
+
+						<h3>Serviços</h3>
+						<span>Seja bem vind@ ao site da ArteSol, se está procurando nossos serviços.</span>
+
+						<a class="btn-artesol bg-marrom-artesol" href="">Clique Aqui</a>
+
+					</div><!-- servicos -->
+
+					<div class="agenda">
+						
+						<h3>Agenda</h3>
+						<span>Participamos e realizamos de diversos eventos e feiras</span>
+
+						<a class="btn-artesol bg-azul-artesol" href="">Veja a Agenda</a>
+
+					</div>
+
 				</div>
 				<div class="col-md-4">
 					Aceleração Social
