ecd9815e32301a9eea51c271ace7e184496e7fa9
everaldomatias <everaldo@brasa.art.br>
2015-04-07 15:48:19 -0300

    Aplicando CSS page.php

--- page.php
+++ page.php
@@ -10,7 +10,7 @@
  * @since 2.2.0
  */
 
-get_header(); ?>
+get_header( 'page' ); ?>
 
 	<div id="primary" class="<?php echo odin_classes_page_full(); ?>">
 		<div id="content" class="site-content" role="main">
