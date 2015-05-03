932538d652ff5970441879b6c78b9b5ed54782d1
Diego Rojas <diego@brasa.art.br>
2015-05-03 00:12:49 -0300

    mais pente fino de css, começada a 404, fix projetos de agenda

--- 404.php
+++ 404.php
@@ -6,16 +6,16 @@
  * @since 2.2.0
  */
 
-get_header(); ?>
+get_header( 'page' ); ?>
 
 	<div id="primary" class="<?php echo odin_classes_page_full(); ?>">
 		<div id="content" class="site-content" role="main">
 
-			<header class="page-header">
-				<h1 class="page-title"><?php _e( 'Not Found', 'odin' ); ?></h1>
+			<header class="entry-header">
+				<h1 class="entry-title">Erro 404 - <?php _e( 'Not Found', 'odin' ); ?></h1>
 			</header>
 
-			<div class="page-content">
+			<div class="entry-content">
 				<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'odin' ); ?></p>
 
 				<?php get_search_form(); ?>
@@ -25,4 +25,4 @@ get_header(); ?>
 	</div><!-- #primary -->
 
 <?php
-get_footer();
+get_footer( 'aceleracao' ); ?>
