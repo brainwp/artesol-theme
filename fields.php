66cd987bfefd92a8825c33ef72edc54a43a3ad73
matheus <contato@matheusgimenez.com.br>
2015-06-10 14:29:20 -0300

    link para conteudo em ingles

--- fields.php
+++ fields.php
@@ -1582,3 +1582,42 @@ if(function_exists("register_field_group"))
 		'menu_order' => 0,
 	));
 }
+if(function_exists("register_field_group"))
+{
+	register_field_group(array (
+		'id' => 'acf_opcoes-da-pagina-em-ingles',
+		'title' => 'Opções da página em inglês',
+		'fields' => array (
+			array (
+				'key' => 'field_55786f628b26a',
+				'label' => 'Link para a página de opções',
+				'name' => 'link_english',
+				'type' => 'text',
+				'default_value' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'formatting' => 'html',
+				'maxlength' => '',
+			),
+		),
+		'location' => array (
+			array (
+				array (
+					'param' => 'options_page',
+					'operator' => '==',
+					'value' => 'acf-options',
+					'order_no' => 0,
+					'group_no' => 0,
+				),
+			),
+		),
+		'options' => array (
+			'position' => 'normal',
+			'layout' => 'default',
+			'hide_on_screen' => array (
+			),
+		),
+		'menu_order' => 0,
+	));
+}
